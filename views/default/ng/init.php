2b1a2832fdea8d7a2d9cede09ec1b303fbeed8ba
Evan Winslow <evan.b.winslow@gmail.com>
2014-08-30 16:17:42 +0000

    fix(init): Add `elgg/http/ng` module to angular bootstrap
    These features were recently split out into a separate angular module.

--- views/default/ng/init.php
+++ views/default/ng/init.php
@@ -4,6 +4,7 @@ define('ng/init', function(require) {
 	
 	angular.bootstrap(document, [
 		// TODO build this list dynamically with PHP for per-app customization
+		require('elgg/http/ng').name,
 		// require('elgg/likes/ng').name,
 		require('elgg/menus/ng').name,
 		require('ng/modules/elgg').name,
