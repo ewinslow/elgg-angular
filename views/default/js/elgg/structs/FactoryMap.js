a0590583ed9f454b3b387a569d24b3b2980fa3db
Evan Winslow <evan.b.winslow@gmail.com>
2014-08-30 05:45:55 +0000

    feature(structs): Rename LazyMap to FactoryMap
    This more accurately reflects what it does (i.e. create new values).

--- views/default/js/elgg/structs/FactoryMap.js
+++ views/default/js/elgg/structs/FactoryMap.js
@@ -1,15 +1,16 @@
 /**
- * LazyMap always returns a value when get() is called.
+ * FactoryMap always returns a value when get() is called.
  * 
- * The default value is whatever is returned by the factory function.
+ * If nothing has already be set() for that key,
+ * the value returned is whatever is returned when the factory function is called.
  */
 define(function(require) {
-	function LazyMap(factory) {
+	function FactoryMap(factory) {
 		this.factory_ = factory;
 		this.items_ = {};
 	}
 	
-	LazyMap.prototype = {
+	FactoryMap.prototype = {
 		get: function(key) {
 			if (!this.items_[key]) {
 				this.set(key, this.factory_());
@@ -24,5 +25,5 @@ define(function(require) {
 	};
 	
 	
-	return LazyMap;
+	return FactoryMap;
 });
--- views/default/js/elgg/structs/FactoryMapTest.js
+++ views/default/js/elgg/structs/FactoryMapTest.js
@@ -1,18 +1,18 @@
 define(function(require) {
-	var LazyMap = require('./LazyMap');
+	var FactoryMap = require('./FactoryMap');
 
-	describe("LazyMap", function() {
+	describe("FactoryMap", function() {
 		it("by default returns the value returned by factory when get() is called", function() {
 			function factory() { return true; }
 			
-			var map = new LazyMap(factory);
+			var map = new FactoryMap(factory);
 			expect(map.get('foo')).toBe(true);
 		});
 		
 		it("returns the same instance on subsequent calls to get() with the same key", function() {
 			function factory() { return new Object(); }
 			
-			var map = new LazyMap(factory);
+			var map = new FactoryMap(factory);
 			var obj1 = map.get('foo');
 			var obj2 = map.get('foo');
 			
@@ -22,7 +22,7 @@ define(function(require) {
 		it("returns new instances on subsequent calls to get() with different keys", function() {
 			function factory() { return new Object(); }
 			
-			var map = new LazyMap(factory);
+			var map = new FactoryMap(factory);
 			
 			var obj1 = map.get('foo');
 			var obj2 = map.get('bar');
@@ -33,7 +33,7 @@ define(function(require) {
 		it("can override particular values with set()", function() {
 			function factory() { return true; }
 			
-			var map = new LazyMap(factory);
+			var map = new FactoryMap(factory);
 			expect(map.get('foo')).toBe(true);
 			
 			map.set('foo', false);
