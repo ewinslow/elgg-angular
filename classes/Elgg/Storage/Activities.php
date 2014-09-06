07b11f086b610a8da659094e833dc1783cd2c36b
Evan Winslow <evan.b.winslow@gmail.com>
2014-09-06 23:31:31 +0000

    feature(storage): Add storage classes for OO representation of database
    These just wrap the global methods for now but allow us to
    keep globals mostly out of new code.

---
+++ classes/Elgg/Storage/Activities.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Elgg\Storage;
+
+class Activities {
+	public function create(array $properties) {
+		return elgg_create_river_item($properties);
+	}
+}
---
+++ classes/Elgg/Storage/Entities.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Elgg\Storage;
+
+use Elgg\Exception;
+
+class Entities {
+	
+	public function get($guid, $type = NULL, $subtype = NULL) {
+		$entity = get_entity($guid);
+		
+		if (!elgg_instanceof($entity, $type, $subtype)) {
+			throw new Exception\NotFound();
+		}
+		
+		return $entity;
+	}
+	
+	public function create(array $attributes) {
+		$type = $attributes['type'];
+		unset($attributes['type']);
+		
+		switch ($type) {
+			case 'object':
+				$entity = new ElggObject();
+				break;
+			case 'user':
+				$entity = new ElggUser();
+				break;
+			case 'group':
+				$entity = new ElggGroup();
+				break;
+			case 'site':
+				$entity = new ElggSite();
+				break;
+			default:
+				throw new Exception("'$type' is not a valid ElggEntity type");
+		}		
+		
+		foreach ($attributes as $property => $value) {
+			$entity->$property = $value;
+		}
+		
+		$entity->save();
+		
+		return $entity;
+	}
+}
